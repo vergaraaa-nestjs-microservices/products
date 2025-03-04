f8c244475b4718538b86ece57f121744a441622a
Edgar Vergara <edgarvm13@hotmail.com>
2025-03-04 09:15:58 +0100

    feat: add product validation functionality to ensure all products exist

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -39,4 +39,9 @@ export class ProductsController {
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
+
+  @MessagePattern({ cmd: 'validate_products' })
+  validateProduct(@Payload() ids: number[]) {
+    return this.productsService.validateProducts(ids);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -74,4 +74,25 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       data: { available: false },
     });
   }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+  }
 }
